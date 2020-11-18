2510ad22c01fe4e6a2043b442994a3d785fce701
amit4mchiba <amit8chiba@gmail.com>
2020-11-18 15:09:08 +0900

    Add files via upload

---
+++ Pbjelly_gap/run_pbjelly.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+source /mnt/md0/PBSuite_15.8.24/setup.sh
+
+#Jelly extraction /mnt/md0/PBSuite_15.8.24/Pbjelly_analysis_9thJuly2018/Protocol.xml
+
+Jelly assembly /mnt/md0/PBSuite_15.8.24/Pbjelly_analysis_9thJuly2018/Protocol.xml -x "--nproc=18"
+
+Jelly.py output /mnt/md0/PBSuite_15.8.24/Pbjelly_analysis_9thJuly2018/Protocol.xml
