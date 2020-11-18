a6333fe3d2fdb418b0d3a3b12a56be31d845ca38
amit4mchiba <amit8chiba@gmail.com>
2020-11-18 15:10:26 +0900

    Update run_pbjelly.sh

--- Pbjelly_gap/run_pbjelly.sh
+++ Pbjelly_gap/run_pbjelly.sh
@@ -2,7 +2,7 @@
 
 source /mnt/md0/PBSuite_15.8.24/setup.sh
 
-#Jelly extraction /mnt/md0/PBSuite_15.8.24/Pbjelly_analysis_9thJuly2018/Protocol.xml
+#Jelly extraction /mnt/md0/PBSuite_15.8.24/Pbjelly_analysis/Protocol.xml
 
 Jelly assembly /mnt/md0/PBSuite_15.8.24/Pbjelly_analysis_9thJuly2018/Protocol.xml -x "--nproc=18"
 
