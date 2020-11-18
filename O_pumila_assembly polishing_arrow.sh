03f6bcf94ca5853e6fa6f574622a7e24b90a62f1
amit4mchiba <amit8chiba@gmail.com>
2020-11-18 15:37:11 +0900

    Update O_pumila_assembly polishing_arrow.sh

--- O_pumila_assembly polishing_arrow.sh
+++ O_pumila_assembly polishing_arrow.sh
@@ -3,82 +3,84 @@
 echo "First round of arrow based polishing begins"
 # first round of arrow polishing
 /mnt/HD1/pacbio/smrtlink/smrtcmds/bin/blasr \
-/mnt/md0/O_pumila_polished_genome/O_pumila_kurabo_novogene_genome_support_all.bam \
-/mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1.fasta \
---bam --out /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_1st_round.bam --nproc 24
+O_pumila_all_PACBIO_raw_Reads.bam \
+O_pumila_genome.fasta \
+--bam --out O_pumila_genome_arrow_1st_round.bam --nproc 24
 
 
 /home/amit8chiba/software/samtools-1.3.1/samtools sort \
--T /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/temp/240718_pumila_pbjelly_hap1_arrow_1st_round.sorted \
-/mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_1st_round.bam \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_1st_round_sorted.bam
+-T O_pumila_genome_arrow_1st_round.sorted \
+O_pumila_genome_arrow_1st_round.bam \
+-o O_pumila_genome_arrow_1st_round_sorted.bam
 
 
-/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/pbindex /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_1st_round_sorted.bam
+/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/pbindex O_pumila_genome_arrow_1st_round_sorted.bam
 
 
-/home/amit8chiba/software/samtools-1.3.1/samtools faidx /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1.fasta
+/home/amit8chiba/software/samtools-1.3.1/samtools faidx O_pumila_genome.fasta
 
 
 
-/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/arrow /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_1st_round_sorted.bam \
---referenceFilename /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1.fasta \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow1_polished.fasta \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow1_polished.gff \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow1_polished.fastq -j 24
+/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/arrow O_pumila_genome_arrow_1st_round_sorted.bam \
+--referenceFilename O_pumila_genome.fasta \
+-o O_pumila_genome_arrow1_polished.fasta \
+-o O_pumila_genome_arrow1_polished.gff \
+-o O_pumila_genome_arrow1_polished.fastq -j 24
 
 echo "Second round of arrow based polishing begins"
 # Second round of arrow polishing
 /mnt/HD1/pacbio/smrtlink/smrtcmds/bin/blasr \
-/mnt/md0/O_pumila_polished_genome/O_pumila_kurabo_novogene_genome_support_all.bam \
-/mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow1_polished.fasta \
---bam --out /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_2nd_round.bam --nproc 24
+O_pumila_all_PACBIO_raw_Reads.bam \
+O_pumila_genome_arrow1_polished.fasta \
+--bam --out O_pumila_genome_arrow_2nd_round.bam --nproc 24
 
 
 /home/amit8chiba/software/samtools-1.3.1/samtools sort \
--T /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/temp/240718_pumila_pbjelly_hap1_arrow_2nd_round.sorted \
-/mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_2nd_round.bam \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_2nd_round_sorted.bam
+-T O_pumila_genome_arrow_2nd_round.sorted \
+O_pumila_genome_arrow_2nd_round.bam \
+-o O_pumila_genome_arrow_2nd_round_sorted.bam
 
 
-/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/pbindex /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_2nd_round_sorted.bam
+/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/pbindex O_pumila_genome_arrow_2nd_round_sorted.bam
 
 
-/home/amit8chiba/software/samtools-1.3.1/samtools faidx /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow1_polished.fasta
+/home/amit8chiba/software/samtools-1.3.1/samtools faidx O_pumila_genome_arrow1_polished.fasta
 
 
 
-/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/arrow /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_2nd_round_sorted.bam \
---referenceFilename /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow1_polished.fasta \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow2_polished.fasta \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow2_polished.gff \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow2_polished.fastq -j 24
+/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/arrow O_pumila_genome_arrow_2nd_round_sorted.bam \
+--referenceFilename O_pumila_genome_arrow1_polished.fasta \
+-o O_pumila_genome_arrow2_polished.fasta \
+-o O_pumila_genome_arrow2_polished.gff \
+-o O_pumila_genome_arrow2_polished.fastq -j 24
 
 
 echo "Third round of arrow based polishing begins"
 # Third round of arrow polishing
 /mnt/HD1/pacbio/smrtlink/smrtcmds/bin/blasr \
-/mnt/md0/O_pumila_polished_genome/O_pumila_kurabo_novogene_genome_support_all.bam \
-/mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow2_polished.fasta \
---bam --out /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_3rd_round.bam --nproc 24
+O_pumila_all_PACBIO_raw_Reads.bam \
+O_pumila_genome_arrow2_polished.fasta \
+--bam --out O_pumila_genome_arrow_3rd_round.bam --nproc 24
 
 
 /home/amit8chiba/software/samtools-1.3.1/samtools sort \
--T /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/temp/240718_pumila_pbjelly_hap1_arrow_3rd_round.sorted \
-/mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_3rd_round.bam \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_3rd_round_sorted.bam
+-T O_pumila_genome_arrow_3rd_round.sorted \
+O_pumila_genome_arrow_3rd_round.bam \
+-o O_pumila_genome_arrow_3rd_round_sorted.bam
 
 
-/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/pbindex /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_3rd_round_sorted.bam
+/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/pbindex O_pumila_genome_arrow_3rd_round_sorted.bam
 
 
-/home/amit8chiba/software/samtools-1.3.1/samtools faidx /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow2_polished.fasta
+/home/amit8chiba/software/samtools-1.3.1/samtools faidx O_pumila_genome_arrow2_polished.fasta
 
 
 
-/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/arrow /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow_3rd_round_sorted.bam \
---referenceFilename /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow2_polished.fasta \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow3_polished.fasta \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow3_polished.gff \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow3_polished.fastq \
--o /mnt/md0/Pumila_genome_17072018/O_pumila_hap1_final/240718_pumila_pbjelly_hap1_arrow3_polished.vcf -j 24
+/mnt/HD1/pacbio/smrtlink/smrtcmds/bin/arrow O_pumila_genome_arrow_3rd_round_sorted.bam \
+--referenceFilename O_pumila_genome_arrow2_polished.fasta \
+-o O_pumila_genome_arrow3_polished.fasta \
+-o O_pumila_genome_arrow3_polished.gff \
+-o O_pumila_genome_arrow3_polished.fastq \
+-o O_pumila_genome_arrow3_polished.vcf -j 24
+
+
